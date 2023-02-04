b7c44fce083afcc32d9f04eb89a0692f7e320255
JacksonMars <jmarsnas@my.bcit.ca>
2023-02-04 14:04:30 -0800

    Create schema for tracked device

---
+++ src/config/schemas/TrackedDevice.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const TrackedDeviceSchema = new Schema({
+    id: {
+        type: Number,
+        unique: true
+    },
+
+    userId: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+
+    deviceId: {
+        type: Number,
+        required: true
+    }
+
+})
+
+
+const TrackedDevice =  mongoose.model('Session', TrackedDeviceSchema);
+
+export default TrackedDevice
