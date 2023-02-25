a3432040134a80c2abd48f989f1e4945d26f6863
susansli <susansemail195@gmail.com>
2023-02-24 19:06:32 -0800

    Fix server file before testing

--- src/server.ts
+++ src/server.ts
@@ -12,11 +12,6 @@ import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-//Load .env (must be loaded ASAP)
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-
 const port = Environment.port;
 const server = express();
 server.use(express.json());
@@ -50,14 +45,10 @@ server.listen(port, async () => {
 import { router as authRouter } from './routes/AuthenticationRoutes';
 import { router as weatherRouter } from './routes/WeatherRoutes';
 import { router as sessionRouter } from './routes/SessionRoutes'
+import { router as apiRouter } from './routes/TimestreamRoutes';
 
 server.use('/api/auth', authRouter);
-
-
-// Testing the timestreamAPI
-import {router as apiRouter} from './routes/TimestreamRoutes';
 server.use('/api/ts', apiRouter)
-
 server.use('/api/weather', weatherRouter);
 server.use('/api/session', sessionRouter);
 
