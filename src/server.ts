9e9a744347e784c4e49198eb250e5b239095d61b
Basil Kim <80417893+Basil-Kim@users.noreply.github.com>
2023-02-24 16:23:19 -0800

    Fix merge conflict

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,11 @@ import cors from 'cors';
 import express from 'express';
 import Environment from './config/Environments';
 import compression from 'compression';
+import mongoose from 'mongoose';
 
 //Load .env (must be loaded ASAP)
 import * as dotenv from 'dotenv';
 dotenv.config();
-
 
 const port = Environment.port;
 const server = express();
@@ -26,13 +26,29 @@ server.use(
     })
 );
 
-server.listen(port, () => {
+server.listen(port, async () => {
+
+    mongoose.set('strictQuery', false);
+    mongoose.connect(`${process.env.MONGO_URL};
+}`);
+    const db = mongoose.connection;
+
+    db.on('error', console.error.bind(console, 'Could not connect to Mongo - restart the server.'));
+    db.once('open', () => {
+        console.log('Connected to MongoDB');
+    });
+
     console.log(`Server started on port ${port}!`);
 });
 
 //Routing
 import { router as authRouter } from './routes/AuthenticationRoutes';
+import { router as weatherRouter } from './routes/WeatherRoutes';
+import { router as sessionRouter } from './routes/SessionRoutes'
+
 server.use('/api/auth', authRouter);
+server.use('/api/weather', weatherRouter);
+server.use('/api/session', sessionRouter);
 
 
 // Testing the timestreamAPI
