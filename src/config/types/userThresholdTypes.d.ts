0ae3658a2bcc4753c7fd421217801f2b36d36006
serguven <90672304+serguven@users.noreply.github.com>
2023-05-02 11:14:49 -0700

    delete unused custom type

--- src/config/types/userThresholdTypes.d.ts
+++ src/config/types/userThresholdTypes.d.ts
@@ -10,15 +10,3 @@ type metric = {
     isWarning: boolean
 }
 
-/////////////////// NEW TYPE USED IN PREHOOOK (SELF) ///////////////////////////
-type userThresholdDoc = {
-    userId: Schema.Types.ObjectId,
-    deviceId: number,
-    // metricList: metricList
-    metricList: {
-        [key: string]: {
-            [key: string]: number | boolean | null
-        }
-    }
-}
-////////////////////////////////////////////////////////////////////////////////
