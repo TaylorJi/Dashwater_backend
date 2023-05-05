31618f1f04412671a9c9e1e855cc4f71ccb91df6
susansli <susansemail195@gmail.com>
2023-05-05 16:01:28 -0700

    Comment out tide cache

--- src/server.ts
+++ src/server.ts
@@ -39,15 +39,15 @@ server.listen(port, async () => {
 
     // Register cache
 
-    console.log('Populating cache with tide data...');
+    // console.log('Populating cache with tide data...');
 
-    const tideRegistration = await AppCache.registerTideCache();
+    // const tideRegistration = await AppCache.registerTideCache();
 
-    if (!tideRegistration) {
-        console.log('There was a problem populating the tide data cache. Check your query limits.');
-    } else {
-        console.log('Populated tide data cache.');
-    }
+    // if (!tideRegistration) {
+    //     console.log('There was a problem populating the tide data cache. Check your query limits.');
+    // } else {
+    //     console.log('Populated tide data cache.');
+    // }
 
     console.log('Populating cache with device data...');
 
