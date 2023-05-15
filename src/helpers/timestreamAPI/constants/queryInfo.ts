aa2768e64319e68aea0dc02ed3ebba4417b20112
Basil Kim <80417893+Basil-Kim@users.noreply.github.com>
2023-05-15 00:39:48 -0700

    Add function to get aws jwt token from cloud secrets manager

--- src/helpers/timestreamAPI/constants/queryInfo.ts
+++ src/helpers/timestreamAPI/constants/queryInfo.ts
@@ -1,17 +1,20 @@
 /**
- * This module exports all information relating to query credentials.
- *
- *      This information is sent with each query.
+ * This module exports all information relating to AWS credentials.
  */
 
 const API_VERSION: string = "2021-11-05";
 const REGION: string = "us-west-2";
 const DATABASE_NAME: string = "YVR_water_sensor";
 const TABLE_NAME: string = "EMA_C22_main_data_v00";
+//TODO: Perhaps move the two below to .env but atm this info isnt set yet
+const VERSION_STAGE: string = "AWSCURRENT";
+const SECRET_NAME: string = "TokenSecret";
 
 export default module.exports = {
   API_VERSION,
   REGION,
   DATABASE_NAME,
   TABLE_NAME,
+  VERSION_STAGE,
+  SECRET_NAME
 };
--- src/models/session/SessionModel.ts
+++ src/models/session/SessionModel.ts
@@ -1,99 +1,140 @@
 import Session from "../../config/schemas/Session";
 import User from "../../config/schemas/User";
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+import queryInfo from "../../helpers/timestreamAPI/constants/queryInfo";
+import { Credentials } from "aws-sdk";
 
 const createSession = async (sessionId: String, userId: String) => {
-    try {
-        await Session.deleteMany({"userId": userId})
-        const expirationTime = new Date();
-        expirationTime.setHours(expirationTime.getHours() + 2);
+  try {
+    await Session.deleteMany({ userId: userId });
+    const expirationTime = new Date();
+    expirationTime.setHours(expirationTime.getHours() + 2);
 
-        const newSession = await Session.create({
-                "userId": userId,
-                "sessionExpiry": expirationTime.toISOString(),
-                "sessionId": sessionId
-        });
+    const newSession = await Session.create({
+      userId: userId,
+      sessionExpiry: expirationTime.toISOString(),
+      sessionId: sessionId,
+    });
 
-        if(newSession) {
-            return newSession;
-        }
-        return null;
-    } catch (_err) {
-        return null;
+    if (newSession) {
+      return newSession;
     }
-}
+    return null;
+  } catch (_err) {
+    return null;
+  }
+};
 
 const deleteSession = async (sessionId: string) => {
-    try {
-        const oldSession = await Session.find({"sessionId": sessionId});
-        if (oldSession) {
-            try {
-                const deletedSession = await Session.deleteOne({"sessionId": sessionId});
+  try {
+    const oldSession = await Session.find({ sessionId: sessionId });
+    if (oldSession) {
+      try {
+        const deletedSession = await Session.deleteOne({
+          sessionId: sessionId,
+        });
 
-                if (deletedSession.deletedCount){
-                    return true;
-                }
-                return false;
-            } catch (err) {
-                return false;
-            }
+        if (deletedSession.deletedCount) {
+          return true;
         }
-
-        return true;
-    } catch (err) {
         return false;
+      } catch (err) {
+        return false;
+      }
     }
-}
+
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
 
 const validateSession = async (sessionId: string) => {
-    const currentTime = new Date();
+  const currentTime = new Date();
 
-    try {
-        const session = await Session.findOne({"sessionId": sessionId});
+  try {
+    const session = await Session.findOne({ sessionId: sessionId });
 
-        if (session) {
-            if(new Date(session.sessionExpiry) < currentTime) {
-                return false;
-            }
+    if (session) {
+      if (new Date(session.sessionExpiry) < currentTime) {
+        return false;
+      }
 
-            const fetchedUser = await User.findOne({"_id": session.userId});
+      const fetchedUser = await User.findOne({ _id: session.userId });
 
-            if (fetchedUser) {
-                const user: userDataType = {
-                    email: fetchedUser['email'], 
-                    userId: fetchedUser['_id'], 
-                    role: fetchedUser['role']
-                };
+      if (fetchedUser) {
+        const user: userDataType = {
+          email: fetchedUser["email"],
+          userId: fetchedUser["_id"],
+          role: fetchedUser["role"],
+        };
 
-                return user;
-            }
-            
-            return null;
-        } else {
-            return null;
-        }
-    } catch (err) {
-        return null;
+        return user;
+      }
+
+      return null;
+    } else {
+      return null;
     }
-}
+  } catch (err) {
+    return null;
+  }
+};
 
 const updateSessionExpiry = async (sessionID: string) => {
-    try {
-        const expirationTime = new Date();
-        expirationTime.setHours(expirationTime.getHours() + 2);
-        const updatedSession = await Session.findOneAndUpdate({"sessionId": sessionID}, {"sessionExpiry": expirationTime.toISOString()});
+  try {
+    const expirationTime = new Date();
+    expirationTime.setHours(expirationTime.getHours() + 2);
+    const updatedSession = await Session.findOneAndUpdate(
+      { sessionId: sessionID },
+      { sessionExpiry: expirationTime.toISOString() }
+    );
 
-        if (updatedSession) {
-            return updatedSession;
-        }
-        return null;
-    } catch (_err) {
-        return null;
+    if (updatedSession) {
+      return updatedSession;
     }
-}
+    return null;
+  } catch (_err) {
+    return null;
+  }
+};
+
+const getAWSToken = async () => {
+  const credentials = new Credentials({
+    accessKeyId: `${process.env.AWS_API_ACCESS_KEY}`,
+    secretAccessKey: `${process.env.SECRET_ACCESS_KEY}`,
+  });
+
+  const client = new SecretsManagerClient({
+    region: queryInfo.REGION,
+    credentials: credentials,
+  });
+
+  try {
+    const response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: queryInfo.SECRET_NAME,
+        VersionStage: queryInfo.VERSION_STAGE,
+      })
+    );
+
+    if (response.SecretString) {
+        const parsedResponse = JSON.parse(response.SecretString);
+        return parsedResponse.key;
+    }
+    return null;
+  } catch (_err) {
+    return null;
+  }
+};
 
 export default module.exports = {
-    createSession,
-    deleteSession,
-    validateSession,
-    updateSessionExpiry
+  createSession,
+  deleteSession,
+  validateSession,
+  updateSessionExpiry,
+  getAWSToken,
 };
