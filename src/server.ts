d0f36362b1f019a7ae9d98082e72e3e677a4a52c
susansli <susansemail195@gmail.com>
2023-05-20 10:57:01 -0700

    Activate tide cache

--- src/server.ts
+++ src/server.ts
@@ -40,17 +40,17 @@ server.listen(port, async () => {
 
     // Register cache
 
-    // console.log('Populating cache with tide data...');
+    console.log('Populating cache with tide data...');
 
-    // const tideRegistration = await AppCache.registerTideCache();
+    const tideRegistration = await AppCache.registerTideCache();
 
-    // if (!tideRegistration) {
-    //     console.log('There was a problem populating the tide data cache. Check your query limits.');
-    // } else {
-    //     console.log('Populated tide data cache.');
-    // }
+    if (!tideRegistration) {
+        console.log('There was a problem populating the tide data cache. Check your query limits.');
+    } else {
+        console.log('Populated tide data cache.');
+    }
 
-    // console.log('Populating cache with device data...');
+    console.log('Populating cache with device data...');
 
     const deviceDataRegistration = await AppCache.registerDeviceCache();
 
@@ -99,5 +99,5 @@ server.use('/api/user', userRouter);
 server.use('/api/trackedDevice', AuthenticationController.userAuth, trackedDeviceRouter);
 server.use('/api/device', AuthenticationController.userAuth, deviceRouter);
 server.use('/api/calibration', AuthenticationController.userAuth, calibrationRouter);
-server.use('/api/defaultThreshold',  AuthenticationController.userAuth,defaultThresholdRouter)
+server.use('/api/defaultThreshold', AuthenticationController.userAuth, defaultThresholdRouter)
 server.use('/api/userThreshold', AuthenticationController.userAuth, userThresholdRouter)
