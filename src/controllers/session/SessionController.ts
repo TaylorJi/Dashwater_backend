832c969d03c079e4a00f8b5b5dbb628791b32ea2
DabeenJung <dabeenj24@gmail.com>
2023-11-29 14:47:10 -0800

    working on validatesession

--- src/controllers/session/SessionController.ts
+++ src/controllers/session/SessionController.ts
@@ -150,7 +150,7 @@ const createSession = async (req: Request, res: Response) => {
 
 const validateSession = async (req: Request, res: Response) => {
     console.log("Validate Session is being called")
-    const sessionToken = req.body.sessionCookie;   
+    const sessionToken = req.body.sessionToken;   
 
     if (sessionToken === undefined || !sessionToken) {
 
