7c03107746fcab4468ad40458350db32205b46e3
DabeenJung <dabeenj24@gmail.com>
2023-11-30 14:48:24 -0800

    testing

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 
 // Middleware
-import AuthenticationController from './controllers/authentication/AuthenticationController'; // Make sure uncomment this when AuthenticationController.userAuth works properly
+// import AuthenticationController from './controllers/authentication/AuthenticationController'; // Make sure uncomment this when AuthenticationController.userAuth works properly
 
 // Cache
 import AppCache from './models/cache/AppCache';
@@ -94,16 +94,22 @@ import { router as userThresholdRouter } from './routes/UserThresholdRoutes'
 
 
 
-// server.use('/api/ts', AuthenticationController.userAuth, timestreamRouter)
-server.use('/api/ts',  AuthenticationController.userAuth, timestreamRouter)
+// server.use('/api/ts',  AuthenticationController.userAuth, timestreamRouter);
+// server.use('/api/weather', AuthenticationController.userAuth, weatherRouter);
+// server.use('/api/session', sessionRouter);
+// server.use('/api/user', userRouter);
+// server.use('/api/trackedDevice', AuthenticationController.userAuth, trackedDeviceRouter);
+// server.use('/api/device', AuthenticationController.userAuth, deviceRouter);
+// server.use('/api/calibration', AuthenticationController.userAuth, calibrationRouter);
+// server.use('/api/defaultThreshold', AuthenticationController.userAuth, defaultThresholdRouter);
+// server.use('/api/userThreshold', AuthenticationController.userAuth, userThresholdRouter);
 
-server.use('/api/weather', AuthenticationController.userAuth, weatherRouter);
+server.use('/api/ts', timestreamRouter);
+server.use('/api/weather', weatherRouter);
 server.use('/api/session', sessionRouter);
 server.use('/api/user', userRouter);
-server.use('/api/trackedDevice', AuthenticationController.userAuth, trackedDeviceRouter);
-server.use('/api/device', AuthenticationController.userAuth, deviceRouter);
-
-// server.use('/api/device', AuthenticationController.userAuth, deviceRouter);
-server.use('/api/calibration', AuthenticationController.userAuth, calibrationRouter);
-server.use('/api/defaultThreshold', AuthenticationController.userAuth, defaultThresholdRouter)
-server.use('/api/userThreshold', AuthenticationController.userAuth, userThresholdRouter)
+server.use('/api/trackedDevice', trackedDeviceRouter);
+server.use('/api/device', deviceRouter);
+server.use('/api/calibration', calibrationRouter);
+server.use('/api/defaultThreshold', defaultThresholdRouter);
+server.use('/api/userThreshold', userThresholdRouter);
