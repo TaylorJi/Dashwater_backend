32f1d739adb4cd6adc0f33621116d05938e3238e
TaylorJi <sji24@my.bcit.ca>
2023-11-29 17:37:54 -0800

    updated userAuth function for testing

--- src/controllers/authentication/AuthenticationController.ts
+++ src/controllers/authentication/AuthenticationController.ts
@@ -4,30 +4,59 @@ import SessionModel from "../../models/session/SessionModel";
 
 // It may be possible to combine userAuth and adminAuth into one function, but it would be harder to understand for future teams
 // One appraoch would be to pass a flag, isAdmin, from the calling route, which would then be passes from controller to model
-const userAuth = async ( req: Request, res: Response, next: NextFunction ) => {
-    console.log("userAuth is being called")
-    console.log("test------------------- " + req.body.sessionToken)
+// const userAuth = async ( req: Request, res: Response, next: NextFunction ) => {
+//     console.log("userAuth is being called")
+//     console.log("test------------------- " + req.body.sessionToken)
 
 
 
-    console.log(req.cookies.sessionCookie)
-    if (!req.cookies.sessionCookie) {
-        res.status(403).json({ message: "No cookie found in request. You must be logged in to perform this action." })
-    } else {
-        const sessionId = req.cookies.sessionCookie.sessionId;
-        if (sessionId) {
-            const sessionCheck = await SessionModel.validateSession(sessionId);
+//     console.log(req.cookies.sessionCookie)
+//     if (!req.cookies.sessionCookie) {
+//         res.status(403).json({ message: "No cookie found in request. You must be logged in to perform this action." })
+//     } else {
+//         const sessionId = req.cookies.sessionCookie.sessionId;
+//         if (sessionId) {
+//             const sessionCheck = await SessionModel.validateSession(sessionId);
     
-            if (sessionCheck) {
-                next();
-            } else {
-                res.status(403).json({ message: "You must be logged in to perform this action." });
-            }
-        } else {
-            res.status(403).json({ message: "You must be logged in to perform this action." });
+//             if (sessionCheck) {
+//                 next();
+//             } else {
+//                 res.status(403).json({ message: "You must be logged in to perform this action." });
+//             }
+//         } else {
+//             res.status(403).json({ message: "You must be logged in to perform this action." });
+//         }
+//     }
+// }
+
+const userAuth = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    console.log("userAuth is being called");
+
+    const authHeader = req.headers.authorization;
+    const sessionToken = authHeader?.split(' ')[1]; // Bearer TOKEN
+
+    console.log("Extracted session token:", sessionToken);
+
+    if (!sessionToken) {
+        res.status(403).json({ message: "No session token found. You must be logged in to perform this action." });
+        return;
+    } 
+
+    try {
+        const sessionCheck = await SessionModel.validateSession(sessionToken);
+        if (!sessionCheck) {
+            res.status(403).json({ message: "Session is invalid or has expired. You must be logged in to perform this action." });
+            return;
         }
+        next();
+    } catch (error) {
+        console.error("Session validation error:", error);
+        res.status(500).json({ message: "Internal server error during session validation." });
     }
-}
+};
+
+
+
 
 const adminAuth = async (req: Request, res: Response, next: NextFunction) => {
     if (!req.cookies.sessionCookie) {
